26a2dfe5bc6ea2b7351f89b2deb146a3da60504a
SumitGaurav <SumitGaurav@Lenovo-PC.sduhsd.lan>
2019-04-24 11:59:54 -0700

    push previous work

---
+++ src/snowman/SnowMan.java
@@ -0,0 +1,31 @@
+package snowman;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+
+public class SnowMan extends AbstractShape
+{
+   public SnowMan(int x, int y, int w, int h )
+   {
+		super(x, y, w, h, Color.WHITE, 0, 0);
+   }
+
+   public void draw(Graphics window)
+   {
+      //add code here to make a snowman 	      	
+       window.fillOval(200, 300, 20, 20);
+       window.fillOval(200, 320, 40, 40);
+       window.fillOval(200, 360, 20, 20);
+       
+   }
+
+   public void moveAndDraw(Graphics window)
+   {
+      draw(window);   
+   }
+}
