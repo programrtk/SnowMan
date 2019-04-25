5dcb886884c090d81535a60d6afce4f40a816c62
SumitGaurav <SumitGaurav@Lenovo-PC>
2019-04-24 19:02:31 -0700

    snowflakes appear and move

--- src/snowman/SnowMan.java
+++ src/snowman/SnowMan.java
@@ -12,7 +12,7 @@ public class SnowMan extends AbstractShape
 {
    public SnowMan(int x, int y, int w, int h )
    {
-		super(x, y, w, h, Color.WHITE, 0, 0);
+	super(x, y, w, h, Color.WHITE, 0, 0);
    }
 
    public void draw(Graphics window)
@@ -20,12 +20,15 @@ public class SnowMan extends AbstractShape
       //add code here to make a snowman 	      	
        window.fillOval(200, 300, 20, 20);
        window.fillOval(200, 320, 40, 40);
-       window.fillOval(200, 360, 20, 20);
+       window.fillOval(200, 360, 60, 60);
        
    }
 
    public void moveAndDraw(Graphics window)
    {
+      draw(window);
+      //setXPos(getXPos() + getXSpeed());
+      setYPos(getYPos() + getYSpeed());
       draw(window);   
    }
 }
